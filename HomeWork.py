930c9af8291d3fe4696387bfb224f702ec538f81
Max <bushkov.niko@mail.ru>
2023-09-24 23:03:14 +0300

    HomeWork2.0

--- HomeWork.py
+++ HomeWork.py
@@ -1,36 +1,38 @@
-while True:
+from fractions import Fraction
 
-    user_input = input('Введите целое число, которое хотите преобразовать: ')
-    res = ''
+str1 = input('Введите дробь в виде a/b: ')
+str2 = input('Введите дробь в виде a/b: ')
 
-    try:
-        user_input = int(user_input)
-    except:
-        print('Что-то не так, попробуйте ещё раз')
-        continue
+number1_str1 = int(str1.split('/')[0])
+number2_str1 = int(str1.split('/')[1])
+number1_str2 = int(str2.split('/')[0])
+number2_str2 = int(str2.split('/')[1])
 
-    while user_input:
-        if user_input % 16 == 10:
-            res = 'a' + res
-            user_input //= 16
-        elif  user_input % 16 == 11:
-            res = 'b' + res
-            user_input //= 16
-        elif  user_input % 16 == 12:
-            res = 'c' + res
-            user_input //= 16
-        elif  user_input % 16 == 13:
-            res = 'd' + res
-            user_input //= 16
-        elif  user_input % 16 == 14:
-            res = 'e' + res
-            user_input //= 16
-        elif  user_input % 16 == 15:
-            res = 'f' + res
-            user_input //= 16
-        else:
-            res = f'{user_input % 16}' + res
-            user_input //= 16
-    else:
-        print(f'0x{res}')
-        break
+if number2_str1 != number2_str2:
+    sum1 = (number1_str1 * number2_str2) + (number1_str2 * number2_str1)
+    sum2 = number2_str1 * number2_str2
+else:
+    sum1 = number1_str1 + number1_str2
+    sum2 = number2_str1
+
+def reduction(number1, number2):
+    for idx in range(2, number1 + 1):
+        while number1 % idx == 0 and number2 % idx == 0:
+            number1 //= idx
+            number2 //= idx
+    return number1, number2
+
+if sum1 == sum2:
+    print(f'{str1} + {str2} = {1}')
+else:
+    sum_lst = reduction(sum1, sum2)
+    print(f'{str1} + {str2} = {sum_lst[0]}/{sum_lst[1]}')
+
+product1 = number1_str1 * number1_str2
+product2 = number2_str1 * number2_str2
+
+if product1 == product2:
+    print(f'{str1} * {str2} = {1}')
+else:
+    product_lst = reduction(product1, product2)
+    print(f'{str1} * {str2} = {product_lst[0]}/{product_lst[1]}')
